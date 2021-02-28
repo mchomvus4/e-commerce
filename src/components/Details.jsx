b09d0d1611e57c993444ae9534e92c4f6945b069
samwelnm4@gmail.com <samwelnm4@gmail.com>
2021-02-28 22:57:46 +0300

    kitchen e-commerce

---
+++ src/components/Details.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Details extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hellow from Details</h3>
+      </div>
+    )
+  }
+}
---
+++ src/data.js
@@ -0,0 +1,107 @@
+
+export const storeProducts =[
+ {
+  id:1,
+  title:"Best Kitchen Utensil Set – 16 Silicone Cooking Utensils.",
+  img:"img/kitchen-1.jpg",
+  price:20.99,
+  company:"Pricepuls",
+  info:"Quality Materials:top-grade silicone heads that are firmly bound to their natural hardwood acacia handles to protect your cookware (no scratches!) and keep you cooking for years.High Quality Silicone designed to protect the non-stick surface of your precious cookware. This will keep your pans & pots safe from scratching. Stress-free cooking!Light and eco-friendly wooden cookware handles provide protection against rust and thawing. The wooden handles have great heat insulation, protecting your hands from scalding",
+  inCart:false ,
+  count:0,
+  total:0
+ },
+  {
+  id:2,
+  title:"Homikit 27 Pieces Silicone Cooking Utensils Set",
+  img:"img/kitchen-2.jpg",
+  price:28.99,
+  company:"Pricepuls",
+  info:"27-Piece Kitchen Cooking Utensils Set is made of superior food-grade silicone with the sturdy stainless steel handles, which is safe and durable to use for years ensuring the health of your family. Silicone Cookware Utensils with Holder can provide all the essential kitchen tools you need for cooking. It would be an excellent addition to your kitchen to make your culinary journey more colorful. Why Choose This Utensils Set? Food Grade Silicone: Made of superior food-grade silicone, non-toxic, environmentally friendly, can be used with confidence. Heat Resistant: Temperature resistant up to 446°F, no melting or deforming, and will not produce harmful substances during cooking. Comfortable in Hand: Ergonomic silicone handles feel cool and comfortable when holding, protect your hands from scalding. Soft & Gentle: Soft surface and flexible edges are gentle for your expensive cookware, protect them from being scratched. Convenient Storage: Hanging loops and the holder allow you to store all your kitchen gadgets easily, save space and keep tidy. Easy to Clean: Non-stick surface and stain-resistant attributes make it easy to clean up by hands in warm water. 27Pcs SILICONE UTENSILS SET WITH HOLDER WILL MEET YOUR NEED FOR COOKIGN 1 x Slotted Spatula 1 x Slotted Turner 1 x Solid Turner 1 x Soup Ladle 1 x Spatula Spoon 1 x Pasta Server 1 x Solid Spoon 1 x Slotted Spoon 1 x Basting Brush 1 x Large Spatula 1 x Spreader Spatula 1 x Skimmer 1 x Kitchen Tong 1 x Egg Whisk 1 x Utensil Holder 2 x Oven Mitts 10 x Measuring Spoons & Cups SWEET TIPS 1. Please wash them with dish soap, and keep them dry after cleaning. 2. Washing by hand recommend, the best way to clean them are gently rinsing in warm water. 3. Do not use wire balls or sharp tools to clean the surface.",
+  inCart:false ,
+  count:0,
+  total:0
+ },
+ {
+  id:3,
+  title:"Measuring Cups and Spoons Set, 8 PCS Stainless",
+  img:"img/kitchen-3.jpg",
+  price:17.99,
+  company:"Pricepuls",
+  info:"Complete with hanging ring to place on a kitchen hook or holder/hanging rack for easier accessibility. Easy to read clear measurements are smartly engraved into the stylish brushed steel handles, to make it easy to measure liquids, powder, foods, drinks, sauces etc. Perfect tool for Baking and Cooking! Features: Measuring cup sizes: 1cup, 1/2cup, 1/3cup, 1/4cup (250ml, 125ml, 80ml & 60ml) Measuring spoons sizes: 1Tbsp, 1Tsp, 1/2Tsp, 1/4Tsp (15ml, 5ml, 2.5ml, 1.25ml). Made of high quality stainless steel, sturdier and more durable. It never melt, chip, warp, or break like plastic models. Newly upgraded double pouring spouts design, ideal for the perfect measure each time, help you to accurately measure out flour, sugar, spices, herbs, oil, vinegar and more. The cups and spoons are all held together through a large stainless steel ring that is easy to take on or off, really good for dry and storage after cooking. Specification: Material: 430 Stainless Steel Color: Rose Gold Weight: 335g Package Size: 19 x 9 x 7cm Package Includes: 4 x Measuring Cups 4 x Measuring Spoons 2 x Stainless Steel Rings 100% SATISFACTION GUARANTEE: PcleasureCD is dedicated to 100% customer satisfaction. We care deeply about our customers and our top priority is to make you satisfy our product. If Measuring Cups and Spoons Set is faulty, damaged or incorrect, or any further problems, please feel free to contact us and we will give you replacement or full refund no-questions-asked!",
+  inCart:false ,
+  count:0,
+  total:0
+ },
+ {
+  id:4,
+  title:"TableCraft Bamboo Handle Wok Spatula, 14.5-Inch",
+  img:"img/kitchen-4.jpg",
+  price:12.00,
+  company:"Pricepuls",
+  info:"Tablecraft 14.5' Bamboo Handle Wok Spatula is ideal for the at home Pan-Asian chef. Easy long handle feature will help remove food without much splatter. The dimensions are 3.75” length x 1.625” width x 14.5” height and the weight is 0.353 LBS.",
+  inCart:false ,
+  count:0,
+  total:0
+ },
+ {
+  id:5,
+  title:"Wooden Spurtle Set Pack of 6, Kitchen Spatula Set,",
+  img:"img/kitchen-5.jpg",
+  price:19.99,
+  company:"Pricepuls",
+  info:"【 Wooden Spatula Set 】: The wooden kitchen utensils set comes with five different kinds of spatulas. These spurtles kitchen tools are perfect for stirring sauces and mixing all the ingredients while cooking Nonstick Utensils Most of the cooks suggest wood spatulas for cooking because wood spatula don’t heat up quickly, soft on cooking surfaces and won’t scratch pots or bowls Bamboo kitchen utensils are more long lasting than other ordinary plastic and silicone spurtle kitchen tool . Have small holes to hang them easily in your kitchen",
+  
+  inCart:false ,
+  count:0,
+  total:0
+ },
+ {
+  id:6,
+  title:"Swissmar Peeler Serrated Blade, Turquoise",
+  img:"img/kitchen-6.jpg",
+  price:11.32,
+  company:"Pricepuls",
+  info:"This isn't just another peeler. Proprietary technology from Switzerland has given our Swiss Classic peelers a few new edges. The stainless steel blade is scalpel sharp-which means less effort to peel and a thinner peel with every stroke. The serrated blade peeler is perfect for more delicate fruits and vegetables such as kiwis, mangoes and tomatoes. A side cutter on each peeler removes eyes and blemishes from produce or can cut grooves in fruits and vegetables. Suitable for left and right handed use, dishwasher safe, Made in Switzerland.",
+  
+  inCart:false ,
+  count:0,
+  total:0
+ },
+ {
+  id:7,
+  title:"Westmark Potato/Apple Grater, 9.5 Inch, Stainless Steel",
+  img:"img/kitchen-7.jpg",
+  price:12.57,
+  company:"Pricepuls",
+  info:"This item is a great manual grater with fine crown blades for preparing potatoes, vegetables, and fruit. Westmark of Germany is well known for its quality craftsmanship of kitchen tools. Westmark has been making reliable kitchenware for over 50 years, and offers a wide range of practical and ingenious kitchen products designed to make cooking easier. Westmark designs and produces many of their products in Elspe, Germany, where a team of highly trained specialists work to produce their amazing kitchen gadgets. Their numerous tools provide for virtually all possible food preparation needs in the kitchen. For kitchenware you can rely on, choose Westmark..",
+  
+  inCart:false ,
+  count:0,
+  total:0
+ },
+ {
+  id:8,
+  title:"Cheese Grater 5 in 1 Rotary Graters-VEKAYA",
+  img:"img/kitchen-8.jpg",
+  price:28.99,
+  company:"Pricepuls",
+  info:"Multiple functions in one tool This rotary cheese grater has 5 Pairs of interchangeable blades make easily for different cooking choice, including fine, coarse shredder,julienne /thin slice and waffle slice. Rotary cheese grater features: Powerful non-slip suction feet for stability with easy release tabs Quick and effortless,easy to assemble and disassemble The rotary cheese grater is dishwasher safe in top rack",
+  
+  inCart:false ,
+  count:0,
+  total:0
+ }
+];
+
+export const detailProduct = {
+ id:1,
+  title:"Best Kitchen Utensil Set – 16 Silicone Cooking Utensils.",
+  img:"img/kitchen-1.jpg",
+  price:20.99,
+  company:"Pricepuls",
+  info:"Quality Materials:top-grade silicone heads that are firmly bound to their natural hardwood acacia handles to protect your cookware (no scratches!) and keep you cooking for years.High Quality Silicone designed to protect the non-stick surface of your precious cookware. This will keep your pans & pots safe from scratching. Stress-free cooking!Light and eco-friendly wooden cookware handles provide protection against rust and thawing. The wooden handles have great heat insulation, protecting your hands from scalding",
+  inCart:false ,
+  count:0,
+  total:0
+}
