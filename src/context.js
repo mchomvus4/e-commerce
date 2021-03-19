25d0a375fd0bfb8a5f108b8957107ca20f283628
samwelnm4@gmail.com <samwelnm4@gmail.com>
2021-03-19 23:40:57 +0300

    context.js updated

--- src/context.js
+++ src/context.js
@@ -63,10 +63,28 @@ const ProductContext = React.createContext()
     })
    }
    increment = (id) =>{
-    console.log('this is increment method');
+    let tempCart =[...this.state.cart];
+    const selectedProduct = tempCart.find(item=> item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count =product.count+1;
+    product.total = product.count * product.price;
+
+      this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotal()})
+
+
+
+
    }
    decrement = (id) =>{
-    console.log('this is decrement method');
+   let tempCart =[...this.state.cart];
+    const selectedProduct = tempCart.find(item=> item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count =product.count-1;
+    product.total = product.count * product.price;
+
+      this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotal()})
    }
    removeItem = (id) =>{
     let tempProduct = [...this.state.products];
